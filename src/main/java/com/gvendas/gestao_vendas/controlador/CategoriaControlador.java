bc089ca579959b25002f804e2d47bd618c182818
Gledson Rodrigues <ggledson@gmail.com>
2024-12-15 12:07:43 -0300

    Implementado o SpringDoc
    Foi substituído o Swagger 2.9.2 pelo SpringDoc 2.7.0

--- src/main/java/com/gvendas/gestao_vendas/controlador/CategoriaControlador.java
+++ src/main/java/com/gvendas/gestao_vendas/controlador/CategoriaControlador.java
@@ -17,11 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gvendas.gestao_vendas.entidades.Categoria;
 import com.gvendas.gestao_vendas.servico.CategoriaServico;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
-@Api(tags = "Categoria")
+@Tag(name = "Categoria", description = "Categoria")
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaControlador {
@@ -29,13 +29,13 @@ public class CategoriaControlador {
 	@Autowired
 	private CategoriaServico categoriaServico;
 
-	@ApiOperation(value = "Listar todas")
+	@Operation(summary = "Listar todas")
 	@GetMapping
 	public List<Categoria> listarTodas() {
 		return categoriaServico.listarTodas();
 	}
 
-	@ApiOperation(value = "Buscar por id")
+	@Operation(summary = "Buscar por id")
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo) {
 		Optional<Categoria> categoria = categoriaServico.buscarPorId(codigo);
@@ -43,14 +43,14 @@ public class CategoriaControlador {
 
 	}
 
-	@ApiOperation(value = "Salvar")
+	@Operation(summary = "Salvar")
 	@PostMapping
 	public ResponseEntity<Categoria> salvar(@Valid @RequestBody Categoria categoria) {
 		Categoria categoriaSalva = categoriaServico.salvar(categoria);
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 
-	@ApiOperation(value = "Atualizar")
+	@Operation(summary = "Atualizar")
 	@PutMapping("/{codigo}")
 	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria) {
 		return ResponseEntity.ok(categoriaServico.atualizar(codigo, categoria));
--- src/main/java/com/gvendas/gestao_vendas/controlador/util/SwaggerConfig.java
+++ src/main/java/com/gvendas/gestao_vendas/controlador/util/SwaggerConfig.java
@@ -1,34 +1,67 @@
 package com.gvendas.gestao_vendas.controlador.util;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
+	public static final String TEST_HEADER = "test-header";
+	
 	@Bean
-	public Docket configuracao() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.gvendas"))
-				.build()
-				.apiInfo(informacaoApi());
+	public GroupedOpenApi configuracao() {
+		return GroupedOpenApi.builder()
+				.group("test-api")
+				.pathsToMatch("/**")
+				.build();
 	}
+	 
+	@Bean
+	public OperationCustomizer customGlobalHeaders() {
 
-	private ApiInfo informacaoApi() {
-		
-		return new ApiInfoBuilder()
+		return (Operation operation, HandlerMethod handlerMethod) -> {
+			Optional<List<Parameter>> isParameterPresent = Optional.ofNullable(operation.getParameters());
+			Boolean isTestHeaderPresent = Boolean.FALSE;
+			if (isParameterPresent.isPresent()) {
+				isTestHeaderPresent = isParameterPresent.get().stream()
+						.anyMatch(param -> param.getName().equalsIgnoreCase(TEST_HEADER));
+			}
+			if (Boolean.FALSE.equals(isTestHeaderPresent)) {
+				Parameter remoteUser = new Parameter().in(ParameterIn.HEADER.toString()).schema(new StringSchema())
+						.name(TEST_HEADER).description("Test Header").required(true);
+				operation.addParametersItem(remoteUser);
+			}
+			return operation;
+		};
+	}
+	
+	@Bean
+	public OpenAPI springOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
 				.title("Gestão de Vendas")
 				.description("Sistema de gestão de vendas")
 				.version("1.0.0")
-				.build();				
+				.license(new License()
+						.name("Dev Team")
+				.url("https://github.com")))
+				.externalDocs(new ExternalDocumentation()
+				.description("Test Documentation")
+				.url("https://github.com"));
 	}
 }
